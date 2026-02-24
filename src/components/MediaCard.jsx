eca8403633aaa14e8128954fb463713859f60f5e
Giancarlo <cooper458.gm@gmail.com>
2026-02-24 12:23:29 +0100

    show rating as 5-star score

--- src/components/MediaCard.jsx
+++ src/components/MediaCard.jsx
@@ -5,6 +5,12 @@ export default function MediaCard({ item }) {
   const originalTitle =
     item.media_type === "tv" ? item.original_name : item.original_title;
 
+  function renderStars(voteAverage) {
+    const stars = Math.ceil(voteAverage / 2); // 0-10 -> 0-5 arrotondato per eccesso
+    const max = 5;
+
+    return "★".repeat(stars) + "☆".repeat(max - stars);
+  }
   return (
     <div className="card h-100 bg-dark text-light border-secondary">
       <div className="card-body">
@@ -23,7 +29,12 @@ export default function MediaCard({ item }) {
           <strong>Lingua:</strong> {item.original_language}
         </p>
         <p className="card-text mb-0">
-          <strong>Voto:</strong> {item.vote_average}
+          <p className="card-text mb-0">
+            <strong>Voto:</strong> {renderStars(item.vote_average)}{" "}
+            <span className="text-secondary">
+              ({item.vote_average.toFixed(1)})
+            </span>
+          </p>
         </p>
       </div>
       <div className="card-footer bg-transparent border-secondary">
