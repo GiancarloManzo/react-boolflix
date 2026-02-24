5cdea95ca0acdac523ff81df8db7d6d11a757349
Giancarlo <cooper458.gm@gmail.com>
2026-02-24 12:19:50 +0100

    add movie posters to media cards

--- src/components/MediaCard.jsx
+++ src/components/MediaCard.jsx
@@ -1,3 +1,5 @@
+const IMAGE_BASE = "https://image.tmdb.org/t/p/w342";
+
 export default function MediaCard({ item }) {
   const title = item.media_type === "tv" ? item.name : item.title;
   const originalTitle =
@@ -8,6 +10,13 @@ export default function MediaCard({ item }) {
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
         <p className="card-text mb-1">
+          {item.poster_path && (
+            <img
+              src={IMAGE_BASE + item.poster_path}
+              alt={title}
+              className="card-img-top"
+            />
+          )}
           <strong>Titolo originale:</strong> {originalTitle}
         </p>
         <p className="card-text mb-1">
